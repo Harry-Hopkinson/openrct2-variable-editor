2b662bf2d6c64bb31fbe1473ace5f195d66cae1b
Harry Hopkinson <harryjhopkinson@gmail.com>
2024-04-15 18:21:33 +0100

    Create UpdateStores function

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import { tabwindow } from "openrct2-flexui";
-import {
-  parkEntranceFee,
-  parkCash,
-  parkRating,
-  bankLoan,
-  guests,
-} from "./stores";
+import { UpdateStores } from "./stores";
 import { ParkTab } from "./tabs/Park";
 import { RideTab } from "./tabs/Ride";
 import { StatsTab } from "./tabs/Stats";
@@ -31,11 +25,7 @@ export function Main() {
     const menuItemName = "Variable Editor";
     ui.registerMenuItem(menuItemName, () => AllWidgets.open());
     context.subscribe("interval.day", () => {
-      parkEntranceFee.set((park.entranceFee / 10).toString());
-      parkCash.set((park.cash / 10).toString());
-      parkRating.set(park.rating.toString());
-      bankLoan.set((park.bankLoan / 10).toString());
-      guests.set(park.guests.toString());
+      UpdateStores();
     });
   }
 }
--- src/stores.ts
+++ src/stores.ts
@@ -9,4 +9,12 @@ let bankLoan = store(`${park.bankLoan / 10}`);
 // Readonly Variables
 let guests = store(`${park.guests}`);
 
+export function UpdateStores() {
+  parkEntranceFee.set((park.entranceFee / 10).toString());
+  parkCash.set((park.cash / 10).toString());
+  parkRating.set(park.rating.toString());
+  bankLoan.set((park.bankLoan / 10).toString());
+  guests.set(park.guests.toString());
+}
+
 export { parkEntranceFee, parkCash, parkRating, bankLoan, guests };
