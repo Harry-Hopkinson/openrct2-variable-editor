30d83682e4716b09fc48bf962a6b92ca046e83ea
Harry Hopkinson <harryjhopkinson@gmail.com>
2024-04-18 21:29:02 +0100

    Bump version to 1.0.2

--- src/info.js
+++ src/info.js
@@ -1,5 +1,5 @@
 export const name = "openrct2-variable-editor";
-export const version = "1.0.1";
+export const version = "1.0.2";
 export const type = "intransient";
 export const license = "MIT";
 
